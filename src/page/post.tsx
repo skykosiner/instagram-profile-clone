604a8ee3ee826279ae2dd300f630047e8c9b72e5
Yoni Kosiner <yoni@kosiner.co.uk>
2022-06-08 16:00:41 +0100

    clean up code

--- src/page/post.tsx
+++ src/page/post.tsx
@@ -3,12 +3,12 @@ import { useParams } from "react-router-dom";
 import { images, image } from "../images";
 
 function Post() {
-    let id = useParams();
+    let { id } = useParams();
 
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
             {images.map((i: image) => {
-                if (i.id === id.id) {
+                if (i.id === id) {
                     return (
                         <div>
                             <img style={{ objectFit: "cover", height: "1000px", width: "900px" }} src={require(`../image-me-daddy/${i.imageName}`)} />
